ef4c0320f4a7310b01f4228504ffa4b43bde9ca3
Ken Ito <ito@infini-ici.co.jp>
2021-04-06 08:44:55 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismTemplate/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PrismTemplate.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
