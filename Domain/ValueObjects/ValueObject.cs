f91b5b6e61845ff3cbb4d9c1981808570f8b6e50
Ken Ito <ito@infini-ici.co.jp>
2021-04-06 21:50:40 +0900

    Add 抽象クラス ValueObject

---
+++ Domain/ValueObjects/ValueObject.cs
@@ -0,0 +1,41 @@
+﻿namespace Domain.ValueObjects
+{
+    public abstract class ValueObject<T> where T : ValueObject<T>
+    {
+        protected abstract bool EqualsCore(T obj);
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return EqualsCore((T)obj);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(ValueObject<T> vo1, ValueObject<T> vo2)
+        {
+            return Equals(vo1, vo2);
+        }
+
+        public static bool operator !=(ValueObject<T> vo1, ValueObject<T> vo2)
+        {
+            return !Equals(vo1, vo2);
+        }
+    }
+}
