08841a42be102dbb60a88f3e6321dec027471423
Ken Ito <ito@infini-ici.co.jp>
2021-04-06 22:28:08 +0900

    Add ExceptionBase

---
+++ Domain/Exceptions/ExceptionBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public abstract class ExceptionBase : Exception
+    {
+        public abstract ExceptionType Type { get; }
+
+        public enum ExceptionType
+        {
+            Error,
+            Warning,
+            Information,
+        }
+    }
+}
