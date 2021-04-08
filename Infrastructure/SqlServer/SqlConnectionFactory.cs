d5cee1135eac9b1557dde23937c9ebf5abdc30be
Ken Ito <ito@infini-ici.co.jp>
2021-04-08 17:51:52 +0900

    Add SqlServer

---
+++ Infrastructure/SqlServer/SqlConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+
+namespace Infrastructure.SqlServer
+{
+    internal static class SqlConnectionFactory
+    {
+        private static readonly string connectionString;
+
+        static SqlConnectionFactory()
+        {
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = "localhost",
+                InitialCatalog = "",
+                IntegratedSecurity = true,
+                Password = "",
+                UserID = "",
+            };
+            connectionString = builder.ToString();
+        }
+
+        internal static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
